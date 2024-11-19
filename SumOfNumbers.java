b8215eaf2c91d595074880f529190578b3410f54
Gokul300304 <07gokul2004@gmail.com>
2024-11-19 19:01:22 +0530

    Creted a java program file for finding the sum of two numbers

---
+++ SumOfNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the first number: ");
+       int a =scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int b =scanner.nextInt();
+        System.out.println("the sum of th two numbers is " + a+b);
+
+    }
+}
